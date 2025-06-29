1d36383c61c9f421371aeb20c91f203d19837fc1
Tasnimul Hasan Mahi <mahitasnimul2@gmail.com>
2025-06-30 03:46:01 +0600

    create transaction overview api

--- src/app/modules/transcation-overview/transaction-overview.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Types } from "mongoose";
-
-export interface TStorage extends Document {
-  storageName: string; // Not Null
-  openingBalance: number; // Not Null, Default: 0
-  openingDate: Date; // Not Null
-  logo?: string; // Nullable
-  companyId: Types.ObjectId;
-}
